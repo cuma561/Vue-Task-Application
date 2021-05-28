63866a49579bd4745ec6075c99eb99ddc631c804
Bartek Krawczak <cuma561@github.com>
2021-05-28 15:32:08 +0200

    Change NewUser.vue files

--- frontend/src/views/User/NewUser.vue
+++ frontend/src/views/User/NewUser.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>New user</h1>
-    <task-form @createOrUpdate="createOrUpdate"></task-form>
+    <user-form @createOrUpdate="createOrUpdate"></user-form>
   </div>
 </template>
 
