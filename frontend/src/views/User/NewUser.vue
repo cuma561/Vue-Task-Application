752eeb3b9ab7d63dc22362f1cbb38b40c21b12ad
Bartek Krawczak <cuma561@github.com>
2021-05-28 15:23:34 +0200

    Add new user view and add routing import new user

---
+++ frontend/src/views/User/NewUser.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>New user</h1>
+    <task-form @createOrUpdate="createOrUpdate"></task-form>
+  </div>
+</template>
+
+<script>
+import userForm from '../../components/UserForm.vue';
+import { api } from '../../helpers/helpers';
+export default {
+  name: 'new-user',
+  components: {
+    'user-form': userForm
+  },
+  methods: {
+    createOrUpdate: async function(user) {
+      await api.createtask(user);
+      this.flash('user created', 'success');
+      this.$router.push(`/users/`);
+    }
+  }
+};
+</script>
